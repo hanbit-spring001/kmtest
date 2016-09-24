27fce679243a3849550ec0271c35a4c5d4a9a4d9
1027 <1027@hb2004>
2016-09-24 15:10:27 +0900

    DI

--- src/main/java/com/hanbit/spring/FirstBean.java
+++ src/main/java/com/hanbit/spring/FirstBean.java
@@ -2,9 +2,13 @@ package com.hanbit.spring;
 
 public class FirstBean {
 	
-	private int value;
+	private SecondBean secondBean;
 	
-	public int getValue() {
-		return value++;
+	public void setSecondBean(SecondBean secondBean) {
+		this.secondBean = secondBean;
+	}
+	
+	public void callSecondBean() {
+		secondBean.printName();
 	}
 }
